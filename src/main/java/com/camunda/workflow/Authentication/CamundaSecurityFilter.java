24299163918a6b4dbbc5008bac34f4f2c20ba339
kondekiran <110444026+kondekiran@users.noreply.github.com>
2023-06-09 22:03:50 +0530

    Update CamundaSecurityFilter.java

--- src/main/java/com/camunda/workflow/Authentication/CamundaSecurityFilter.java
+++ src/main/java/com/camunda/workflow/Authentication/CamundaSecurityFilter.java
@@ -1,19 +1,20 @@
+
 @Configuration
 public class CamundaSecurityFilter {
 
-	@Bean
-	public FilterRegistrationBean processEngineAuthenticationFilter() {
-		FilterRegistrationBean registration = new FilterRegistrationBean();
-		registration.setName("camunda-auth");
-		registration.setFilter(getProcessEngineAuthenticationFilter());
-		registration.addInitParameter("authentication-provider",
-				"org.camunda.bpm.engine.rest.security.auth.impl.HttpBasicAuthenticationProvider");
-		registration.addUrlPatterns("/*");
-		return registration;
-	}
+    @Bean
+    public FilterRegistrationBean<Filter> processEngineAuthenticationFilter() {
+        FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+        registration.setName("camunda-auth");
+        registration.setFilter(getProcessEngineAuthenticationFilter());
+        registration.addInitParameter("authentication-provider",
+                "org.camunda.bpm.engine.rest.security.auth.impl.HttpBasicAuthenticationProvider");
+        registration.addUrlPatterns("/*");
+        return registration;
+    }
 
-	@Bean
-	public Filter getProcessEngineAuthenticationFilter() {
-		return new ProcessEngineAuthenticationFilter();
-	}
+    @Bean
+    public Filter getProcessEngineAuthenticationFilter() {
+        return new ProcessEngineAuthenticationFilter();
+    }
 }
