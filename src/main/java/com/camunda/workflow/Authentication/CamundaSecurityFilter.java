0894af5564c29f04c258716f82318c6725891d4c
kondekiran <110444026+kondekiran@users.noreply.github.com>
2023-06-13 23:33:40 +0530

    Update CamundaSecurityFilter.java

--- src/main/java/com/camunda/workflow/Authentication/CamundaSecurityFilter.java
+++ src/main/java/com/camunda/workflow/Authentication/CamundaSecurityFilter.java
@@ -4,26 +4,22 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import javax.servlet.Filter;
 import org.camunda.bpm.engine.rest.security.auth.ProcessEngineAuthenticationFilter;
 
+
 @Configuration
-public class CamundaSecurityConfig
-{
+public class CamundaSecurityFilter {
 
-	@Bean
-	public FilterRegistrationBean<ProcessEngineAuthenticationFilter> processEngineAuthenticationFilter()
-	{
-		FilterRegistrationBean<ProcessEngineAuthenticationFilter> registration = new FilterRegistrationBean<>();
-		registration.setName("camunda-auth");
-		registration.setFilter(this.getProcessEngineAuthenticationFilter());
-		registration
-			.addInitParameter("authentication-provider", HttpBasicAuthenticationProvider.class.getName());
-		registration.addUrlPatterns("/engine-rest/*");
-		return registration;
-	}
+    @Bean
+    public FilterRegistrationBean<Filter> processEngineAuthenticationFilter() {
+        FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+        registration.setName("camunda-auth");
+        registration.setFilter(getProcessEngineAuthenticationFilter());
+        registration.addInitParameter("authentication-provider", "org.camunda.bpm.engine.rest.security.auth.impl.HttpBasicAuthenticationProvider");
+        registration.addUrlPatterns("/engine/default/user"); // Specify the endpoint where authentication is required
+        return registration;
+    }
 
-	@Bean
-	public ProcessEngineAuthenticationFilter getProcessEngineAuthenticationFilter()
-	{
-		return new ProcessEngineAuthenticationFilter();
-	}
+    @Bean
+    public Filter getProcessEngineAuthenticationFilter() {
+        return new ProcessEngineAuthenticationFilter();
+    }
 }
-
