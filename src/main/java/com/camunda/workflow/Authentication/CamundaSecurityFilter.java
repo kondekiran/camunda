cfa0a8ef37d9fc21955d3c733f73e50000fec2c5
kondekiran <110444026+kondekiran@users.noreply.github.com>
2023-06-13 15:56:55 +0530

    Update CamundaSecurityFilter.java

--- src/main/java/com/camunda/workflow/Authentication/CamundaSecurityFilter.java
+++ src/main/java/com/camunda/workflow/Authentication/CamundaSecurityFilter.java
@@ -3,34 +3,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import javax.servlet.Filter;
 import org.camunda.bpm.engine.rest.security.auth.ProcessEngineAuthenticationFilter;
-
 @Configuration
-public class CamundaSecurityFilter {
+public class CamundaSecurityConfig
+{
 
-  
-public class CamundaSecurityFilter {
+	@Bean
+	public FilterRegistrationBean<ProcessEngineAuthenticationFilter> processEngineAuthenticationFilter()
+	{
+		FilterRegistrationBean<ProcessEngineAuthenticationFilter> registration = new FilterRegistrationBean<>();
+		registration.setName("camunda-auth");
+		registration.setFilter(this.getProcessEngineAuthenticationFilter());
+		registration
+			.addInitParameter("authentication-provider", HttpBasicAuthenticationProvider.class.getName());
+		registration.addUrlPatterns("/engine-rest/*");
+		return registration;
+	}
 
-    @Value('${camunda.rest-api.jwt.secret-path}')
-    String jwtSecretPath;
-
-    @Value('${camunda.rest-api.jwt.validator-class}')
-    String jwtValidatorClass;
-
-    @Bean
-    public FilterRegistrationBean processEngineAuthenticationFilter() {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setName("camunda-jwt-auth");
-        registration.addInitParameter('authentication-provider', 'io.digitalstate.camunda.authentication.jwt.AuthenticationFilterJwt');
-        registration.addInitParameter('jwt-secret-path', jwtSecretPath);
-        registration.addInitParameter('jwt-validator', jwtValidatorClass);
-        registration.addUrlPatterns("/rest/*");
-        registration.setFilter(getProcessEngineAuthenticationFilter());
-        return registration;
-    }
-
-    @Bean
-    public Filter getProcessEngineAuthenticationFilter() {
-        return new ProcessEngineAuthenticationFilterJwt();
-    }
+	@Bean
+	public ProcessEngineAuthenticationFilter getProcessEngineAuthenticationFilter()
+	{
+		return new ProcessEngineAuthenticationFilter();
+	}
 }
 
